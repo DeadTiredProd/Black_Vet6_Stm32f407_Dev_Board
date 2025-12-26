7882932203fb62380a5ef977f2a3efdebc5eeb76
DeadTiredProd <hellfire2657@gmail.com>
2025-12-26 18:20:49 -0500

    Added USB CDC  debugging , disabled SD card

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -18,18 +18,8 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "stm32f407xx.h"
-#include "stm32f4xx_hal_gpio.h"
-
-/* -------------------------------------------------------------------------- */
-/* If you want SDIO initialization to run, define ENABLE_SDIO = 1 somewhere   */
-/* (project settings or before including this file). Default is disabled so   */
-/* that HAL_SD_Init won't force Error_Handler when MspInit isn't provided.    */
-/* -------------------------------------------------------------------------- */
-#ifndef ENABLE_SDIO
-#define ENABLE_SDIO 0
-#endif
-
+#include "usb_device.h"
+#include "usbd_cdc_if.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -99,13 +89,29 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  //MX_SDIO_SD_Init();
+  MX_USB_DEVICE_Init();
 
-  /* NOTE: SDIO init is disabled by default to avoid jumping to Error_Handler
-     if the HAL SD MSP functions / pin configuration are not present.
-     To enable real SDIO init, define ENABLE_SDIO = 1 in your project. */
-#if ENABLE_SDIO
-  MX_SDIO_SD_Init();
-#endif
+ 
+  /* Make sure SysTick is highest priority */
+  HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+  /* Lower USB OTG FS IRQ priority to prevent it from starving main loop */
+  HAL_NVIC_SetPriority(OTG_FS_IRQn, 6, 0);
+
+   /* --- FIXES ADDED HERE --- */
+  /* Ensure SystemCoreClock is updated and SysTick is configured properly in case USB init changed clocks */
+  SystemCoreClockUpdate();
+  HAL_InitTick(TICK_INT_PRIORITY);  // Re-initialize SysTick for HAL_Delay
+
+  /* Re-initialize LED GPIOs in case USB init changed them */
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  GPIO_InitStruct.Pin = LED1_Pin|LED2_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+  HAL_GPIO_WritePin(GPIOA, LED1_Pin|LED2_Pin, GPIO_PIN_RESET);
 
   /* USER CODE BEGIN 2 */
 
@@ -113,18 +119,19 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
+ while(1)
 {
-    /* LED1 ON, LED2 OFF */
-    HAL_GPIO_WritePin(GPIOA, LED1_Pin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(GPIOA, LED2_Pin, GPIO_PIN_RESET);
-    HAL_Delay(250);
+    /* Toggle both LEDs */
+    HAL_GPIO_TogglePin(GPIOA, LED1_Pin | LED2_Pin);
 
-    /* LED1 OFF, LED2 ON */
-    HAL_GPIO_WritePin(GPIOA, LED1_Pin, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(GPIOA, LED2_Pin, GPIO_PIN_SET);
-    HAL_Delay(250);
+    /* Send USB CDC message */
+    char msg[] = "Hello from STM32!\r\n";
+    CDC_Transmit_FS((uint8_t*)msg, sizeof(msg)-1);
+
+    /* Wait 500 ms */
+    HAL_Delay(50);
 }
+
 
   /* USER CODE END WHILE */
 
@@ -141,39 +148,36 @@ void SystemClock_Config(void)
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /* Supply configuration update enable */
+  /** Configure the main internal regulator output voltage
+  */
   __HAL_RCC_PWR_CLK_ENABLE();
   __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /* Configure HSE and PLL: HSE=8MHz -> PLLM=8, PLLN=336, PLLP=2, PLLQ=7
-     VCO_IN = 1 MHz, VCO_OUT = 336 MHz
-     SYSCLK = 336/2 = 168 MHz
-     48MHz domain = 336/7 = 48 MHz (for SDIO/USB) */
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM       = 8;
-  RCC_OscInitStruct.PLL.PLLN       = 336;
-  RCC_OscInitStruct.PLL.PLLP       = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ       = 7;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
   }
 
-  /* Set clocks dividers:
-     AHB  = SYSCLK / 1  = 168 MHz
-     APB1 = HCLK   / 4  = 42  MHz (max)
-     APB2 = HCLK   / 2  = 84  MHz */
-  RCC_ClkInitStruct.ClockType      = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
-                                     RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;   /* <<-- was DIV2 in your code: BAD */
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  /* IMPORTANT: FLASH_LATENCY_5 is required for 168 MHz on F407 (Scale1) */
   if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
   {
     Error_Handler();
@@ -184,20 +188,9 @@ void SystemClock_Config(void)
   * @brief SDIO Initialization Function
   * @param None
   * @retval None
-  *
-  * Note: function remains present so you can enable SDIO by defining ENABLE_SDIO.
-  * Make sure you implement HAL_SD_MspInit and configure SDIO pins before enabling.
   */
 static void MX_SDIO_SD_Init(void)
 {
-
-  /* USER CODE BEGIN SDIO_Init 0 */
-
-  /* USER CODE END SDIO_Init 0 */
-
-  /* USER CODE BEGIN SDIO_Init 1 */
-
-  /* USER CODE END SDIO_Init 1 */
   hsd.Instance = SDIO;
   hsd.Init.ClockEdge = SDIO_CLOCK_EDGE_RISING;
   hsd.Init.ClockBypass = SDIO_CLOCK_BYPASS_DISABLE;
@@ -213,10 +206,6 @@ static void MX_SDIO_SD_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN SDIO_Init 2 */
-
-  /* USER CODE END SDIO_Init 2 */
-
 }
 
 /**
@@ -227,9 +216,6 @@ static void MX_SDIO_SD_Init(void)
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-  /* USER CODE BEGIN MX_GPIO_Init_1 */
-
-  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOH_CLK_ENABLE();
@@ -240,21 +226,13 @@ static void MX_GPIO_Init(void)
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, LED1_Pin|LED2_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED1_Pin LED2_Pin (PA6, PA7 expected) */
+  /*Configure GPIO pins : LED1_Pin LED2_Pin */
   GPIO_InitStruct.Pin = LED1_Pin|LED2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /* USER CODE BEGIN MX_GPIO_Init_2 */
-
-  /* USER CODE END MX_GPIO_Init_2 */
 }
-
-/* USER CODE BEGIN 4 */
-
-/* USER CODE END 4 */
 
 /**
   * @brief  This function is executed in case of error occurrence.
@@ -262,17 +240,15 @@ static void MX_GPIO_Init(void)
   */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
   __disable_irq();
   while (1)
   {
   }
-  /* USER CODE END Error_Handler_Debug */
 }
+
 #ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
+  * @brief  Reports the name of the source file and the source line number,
   *         where the assert_param error has occurred.
   * @param  file: pointer to the source file name
   * @param  line: assert_param error line source number
@@ -280,9 +256,5 @@ void Error_Handler(void)
   */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
